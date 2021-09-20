fb68917fe89f38cc9c1369704c0e375f44403ac2
Joey Sachtleben <manjoey7@gmail.com>
2021-09-20 12:15:48 -0500

    Split class code into dedicated cpp file

--- SafeArrayInt.cpp
+++ SafeArrayInt.cpp
@@ -1,1 +1,82 @@
 #include "SafeArrayInt.h"
+
+void SafeArrayInt::doubleArraySize()
+{
+	const int newCap = arrayTotalCapacity * 2;
+	int* newArray = new int[newCap];
+
+	for (int i = 0; i < arrayUsedCapacity; ++i)
+	{
+		newArray[i] = intArray[i];
+	}
+		
+	delete [] intArray;
+	intArray = newArray;
+	arrayTotalCapacity = newCap;
+}
+
+void SafeArrayInt::halveArraySize()
+{
+	const int newCap = arrayTotalCapacity / 2;
+	int* newArray = new int[newCap];
+
+	assert(arrayUsedCapacity <= (newCap / 2) && "Trying to halve array when more than half of the new array would be used!");
+		
+	for (int i = 0; i < arrayUsedCapacity; ++i)
+	{
+		newArray[i] = intArray[i];
+	}
+
+	delete [] intArray;
+	intArray = newArray;
+	arrayTotalCapacity = newCap;
+}
+
+SafeArrayInt::SafeArrayInt()  : arrayTotalCapacity(10), arrayUsedCapacity(0), intArray(new int[arrayTotalCapacity])
+{
+	intArray[0] = 0;
+}
+
+
+// ReSharper disable once CppInconsistentNaming
+void SafeArrayInt::push_back(const int newValue)
+{
+	if (arrayTotalCapacity == arrayUsedCapacity)
+		doubleArraySize();
+
+	intArray[arrayUsedCapacity] = newValue;
+	arrayUsedCapacity++;
+}
+
+// ReSharper disable once CppInconsistentNaming
+int SafeArrayInt::pop_back()
+{
+	const int retVal = intArray[arrayUsedCapacity];
+	intArray[arrayUsedCapacity] = 0;
+	arrayUsedCapacity--;
+
+	if ((arrayUsedCapacity <= (arrayTotalCapacity / 4)) && (arrayTotalCapacity > 10))
+		halveArraySize();
+		
+	return retVal;
+}
+
+
+int& SafeArrayInt::at(const int pos) const
+{
+	if (pos >= 0 && pos <= (arrayUsedCapacity - 1))
+	{
+		return intArray[pos];
+	}
+	else
+	{
+		std::cerr << "Error! Tried to access element " << pos << " which is outside the array bounds.\n";
+		exit(1);
+	}
+}
+
+SafeArrayInt::~SafeArrayInt()
+{
+	delete [] intArray;
+	std::cout << "Deleted intArray from the SafeArrayInt!\n";
+}
--- SafeArrayInt.h
+++ SafeArrayInt.h
@@ -12,93 +12,26 @@ private:
 	int* intArray;
 
 
-	void doubleArraySize()
-	{
-		const int newCap = arrayTotalCapacity * 2;
-		int* newArray = new int[newCap];
-
-		for (int i = 0; i < arrayUsedCapacity; ++i)
-		{
-			newArray[i] = intArray[i];
-		}
-		
-		delete [] intArray;
-		intArray = newArray;
-		arrayTotalCapacity = newCap;
-	}
-
-	void halveArraySize()
-	{
-		const int newCap = arrayTotalCapacity / 2;
-		int* newArray = new int[newCap];
-
-		assert(arrayUsedCapacity <= (newCap / 2) && "Trying to halve array when more than half of the new array would be used!");
-		
-		for (int i = 0; i < arrayUsedCapacity; ++i)
-		{
-			newArray[i] = intArray[i];
-		}
-
-		delete [] intArray;
-		intArray = newArray;
-		arrayTotalCapacity = newCap;
-	}
+	void doubleArraySize();
+	void halveArraySize();
 
 public:
-	SafeArrayInt() : arrayTotalCapacity(10), arrayUsedCapacity(0), intArray(new int[arrayTotalCapacity])
-	{
-		intArray[0] = 0;
-	}
+	SafeArrayInt();
 
 	int size() const { return arrayUsedCapacity; }
 	int getTotalCap() const { return arrayTotalCapacity; }
 
 	// ReSharper disable once CppInconsistentNaming
-	void push_back(const int newValue)
-	{
-		if (arrayTotalCapacity == arrayUsedCapacity)
-			doubleArraySize();
-
-		intArray[arrayUsedCapacity] = newValue;
-		arrayUsedCapacity++;
-	}
+	void push_back(const int newValue);
 
 	// ReSharper disable once CppInconsistentNaming
-	int pop_back()
-	{
-		const int retVal = intArray[arrayUsedCapacity];
-		intArray[arrayUsedCapacity] = 0;
-		arrayUsedCapacity--;
+	int pop_back();
 
-		if ((arrayUsedCapacity <= (arrayTotalCapacity / 4)) && (arrayTotalCapacity > 10))
-			halveArraySize();
-		
-		return retVal;
-	}
+	int& at(const int pos) const;
 
-	int& at(const int pos) const
-	{
-		if (pos >= 0 && pos <= (arrayUsedCapacity - 1))
-		{
-			return intArray[pos];
-		}
-		else
-		{
-			std::cerr << "Error! Tried to access element " << pos << " which is outside the array bounds.\n";
-			exit(1);
-		}
-	}
+	int& operator[](const int pos) const { return at(pos); }
 
-	int& operator[](const int pos) const
-	{
-		return at(pos);
-	}
-
-	~SafeArrayInt()
-	{
-		delete [] intArray;
-		std::cout << "Deleted intArray from the SafeArrayInt!\n";
-	}
+	~SafeArrayInt();
 	
 };
 
